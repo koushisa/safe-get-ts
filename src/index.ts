0d7e04acbc8913fe77396d890a3b4033da3f1fb4
Shinya Komatsu <31304738+koushisa@users.noreply.github.com>
2022-08-22 09:53:58 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { safeGet } from "./safeGet/safeGet";
 
-const nestedObj = { nest1: { nest2: 2 } };
-const arr = [nestedObj, nestedObj, { hoge: 666 }] as const;
-const obj = { top: nestedObj, topNest2: "4", arr };
+const nestedObj = { key: "nestedObj", nest1: { nest2: 2 } };
+const arr = [nestedObj, nestedObj, { hoge: "hoge" }] as const;
+
+const obj = { root: nestedObj, root2: "root2", arr };
 
 const value = safeGet(obj, "arr.2.hoge");
 
